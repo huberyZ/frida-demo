7fd5fe7b0be448a4f00f2f755296145cab4e1cd3
nathan <zyc325129@gmail.com>
2019-07-01 22:19:20 +0800

    添加native函数hook

--- frida_test.py
+++ frida_test.py
@@ -2,7 +2,7 @@
 import frida
 import sys
 
-jscode = """
+javaFuncJsCode = """
 if(Java.available){
     Java.perform(function(){
         var MainActivity = Java.use("com.test.myimei.MainActivity");
@@ -15,17 +15,52 @@ if(Java.available){
 }
 """
 
+
+privateNativeFuncJsCode = """
+var nativePointer = new NativePointer(0x111111); // 此处参数为私有函数地址
+send("native pointers:" + nativePointer);
+var resultPointer;
+
+Interceptor.attach(nativePointer, {
+    onEnter: function(args) {
+    
+    },
+
+    onLeave: function(retval) {
+    
+    }
+});
+"""
+
+exportNativeFuncJsCode = """
+var nativePointer = Module.findExportByName("libjniFunc.so", "Java_com_test_myimei_MainActivity_encryption");
+send("native func encrytion() pointers:" + nativePointer);
+Interceptor.attach(nativePointer, {
+    onEnter: function(args) {
+        send("encrytion() args: " + args[0])
+    },
+    onLeave: function(retval) {
+        send("encrytion result value:" + retval);
+    }
+});
+"""
+
 def on_message(message, data):
     if message['type'] == 'send':
         print(" {0}".format(message['payload']))
     else:
         print(message)
 
-rdev = frida.get_remote_device()
-session = rdev.attach("com.test.myimei")
-script = session.create_script(jscode)
-script.on('message', on_message)
-print(' Start attach')
-script.load()
-sys.stdin.read()
+def startHook(packageName, jsCode):
+    rdev = frida.get_remote_device()
+    session = rdev.attach(packageName)
+    script = session.create_script(jsCode)
+    script.on('message', on_message)
+    print(' Start attach')
+    script.load()
+    sys.stdin.read()
 
+if __name__ == "__main__":
+#    startHook("com.test.myimei", javaFuncJsCode)
+    startHook("com.test.myimei", exportNativeFuncJsCode)
+   
